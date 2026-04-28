f608d6488f7a2577de7cbfdb5b168f19c6f76283
bowlsbot <chiltonweb25@gmail.com>
2026-04-28 19:32:18 +0100

    Implement API GET and POST functions

--- src/api/api.js
+++ src/api/api.js
@@ -1,1 +1,25 @@
+const API_URL = "http://<pi-ip>:4000/api";
+
+function token() {
+  return localStorage.getItem("playerToken") || "";
+}
+
+export async function apiGet(path) {
+  const res = await fetch(`${API_URL}${path}`, {
+    headers: { "x-player-token": token() },
+  });
+  return res.json();
+}
+
+export async function apiPost(path, body) {
+  const res = await fetch(`${API_URL}${path}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "x-player-token": token(),
+    },
+    body: JSON.stringify(body),
+  });
+  return res.json();
+}
 
